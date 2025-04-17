f98979af16c8672c4050c8bef2e5a20ff9cfaa3f
Michila0 <michila.bimsara12@gmail.com>
2025-04-17 17:11:33 +0530

    chore: create the new sqlite

--- src/dao/initDB.js
+++ src/dao/initDB.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database.sqlite');
+const db = new sqlite3.Database('./db.sqlite');
 
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS users (
