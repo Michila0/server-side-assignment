2260f3c1ee0c8e905851894072a28cdb2d6f4c63
Michila0 <michila.bimsara12@gmail.com>
2025-04-17 10:50:31 +0530

    chore: change the app.js

--- src/app.js
+++ src/app.js
@@ -1,29 +1,14 @@
-// src/app.js
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const authRoutes = require('./routes/auth');
-const countryRoutes = require('./routes/country');
-const apiKeyRoutes = require('./models/apiKey');
-const authenticate = require('./middleware/auth');
-const dotenv = require('dotenv');
-
-dotenv.config();
+require("dotenv").config();
+const express = require("express");
 const app = express();
-app.use(express.json());
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+const db = require("./dao/database");
 
-// Database connection
-const db = new sqlite3.Database('./database.sqlite', (err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.message);
-    } else {
-        console.log('Connected to the SQLite database.');
-    }
-});
-
-// Routes
-app.use('/auth', authRoutes);
-app.use('/api', authenticate, countryRoutes);
-app.use('/api', authenticate, apiKeyRoutes);
+app.use(bodyParser.json());
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.use("/api", routes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
